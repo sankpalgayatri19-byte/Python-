1d3dbb1bd41cfb4b9303009939cd15ff705bced7
sankpalgayatri19-byte <sankpalgayatri19@gmail.com>
2026-08-13 13:47:40 +0530

    Create slicingstr.py

---
+++ slicingstr.py
@@ -0,0 +1,19 @@
+str1 = 'Hello!!
+str2 = 'Goodbye!!'
+
+print(str1)
+print(str2)
+
+
+
+
+#========================================================================================
+
+str1 = 'Hello!!'
+str2 = 'GOOdbye'
+
+if str1 == 'Hello!!'
+  print(str1)
+else:
+  print(str2)
+  
