dc90b4493cf8414ca44bd7e557d20f81ea74a8bc
sankpalgayatri19-byte <sankpalgayatri19@gmail.com>
2026-08-18 22:06:00 +0530

    Create practicepython.c

---
+++ practicepython.c
@@ -0,0 +1,14 @@
+
+name = input("Enter your name: ")
+  
+print(f"Hello, {name}! Let's do some math.")
+num1 = 10
+num2 = 5
+  
+total = num1 + num2
+print(f"The sum of {num1} and {num2} is: {total}")
+
+if total % 2 == 0:
+    print("The total is an even number.")
+else:
+    print("The total is an odd number.")
